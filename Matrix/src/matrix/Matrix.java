dd351076b32c1e70765570a88c56aea9b8dcb19e
danaantal <danaantal@gmail.com>
2014-03-21 17:40:43 +0200

    trying...

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -3,8 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package matrix;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -15,8 +20,42 @@ public class Matrix {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        int row, column;
+        int[][] d = {{1, 2, 3, 2, 2}, {4, 5, 6, 5, 6}, {9, 1, 3, 4, 4}, {3, 3, 3, 3, 3}, {1, 5, 6, 7, 4}};
+        int x = 5, y = 5;
+        Scanner scan = new Scanner(new FileReader("matrix.txt"));
+        row = scan.nextInt();
+        column = scan.nextInt();
+        int[][] m = new int[row][column];
+
+        MatrixClass matrix = new MatrixClass(row, column);
+        matrix.setColumn(column);//setam coloana
+        matrix.setRow(row); //setam linie
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                m[i][j] = scan.nextInt();
+            }
+        }
+        matrix.setMatrix(m);
+
+        for (int i = 0; i < matrix.getRow(); i++) {
+            System.out.println("");
+            for (int j = 0; j < matrix.getColumn(); j++) {
+                System.out.print(matrix.getMatrix()[i][j] + " ");
+            }
+        }
+        System.out.println();
+
+        MatrixClass M = matrix.multiplyingM(x, y, d);
+        for (int i = 0; i < M.getRow(); i++) {
+            System.out.println("");
+            for (int j = 0; j < M.getColumn(); j++) {
+                System.out.print(M.getMatrix()[i][j] + " ");
+            }
+        }
+        
+
     }
-    
 }
--- Matrix/src/matrix/MatrixClass.java
+++ Matrix/src/matrix/MatrixClass.java
@@ -11,29 +11,146 @@ package matrix;
  */
 public class MatrixClass {
 
-    int row, column, r;
-    int[] vector = new int[r];
-    int[][] matrix = new int[row][column];
+    private int row, column, r;
+    private int[] vector;
+    private int[][] matrix;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    public int[] getVector() {
+        return vector;
+    }
+
+    public void setVector(int[] vector) {
+        this.vector = vector;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
 
     public MatrixClass(int r1) {
         r = r1;
-        int[] vector1 = new int[r1];
-        
-        for(int i : vector1){
-            vector[i] = vector1[i];
-        }
+        vector = new int[r];
     }
 
     public MatrixClass(int r, int c) {
         row = r;
         column = c;
-        int[][] matrix1 = new int[r][c];
-        
-        for(int[] i : matrix1){
-            for(int j : i){
-                matrix[j] = matrix1[j];
+        matrix = new int[row][column];
+
+    }
+
+    public MatrixClass(MatrixClass M) {
+        M = this;
+    }
+
+    private int[][] addingNo(int x) { //adunam numar
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                matrix[i][j] += x;
             }
         }
+        return matrix;
+    }
 
+    private int[][] decreasingNo(int x) { //scadem numar
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                matrix[i][j] -= x;
+            }
+        }
+        return matrix;
+    }
+
+    private int[][] addingM(int r, int c, int[][] m) throws Exception { //adaugam matrice
+        m = new int[r][c];
+        if (r != row && c != column) {
+            throw new Exception();
+        } else {
+            for (int i = 0; i < this.matrix.length; i++) {
+                for (int j = 0; j < this.matrix[0].length; j++) {
+                    matrix[i][j] += m[i][j];
+                }
+            }
+        }
+        return matrix;
+    }
+
+    private int[][] decreasingM(int r, int c, int[][] m) throws Exception { //scadem matrice
+        m = new int[r][c];
+        if (r != row && c != column) {
+            throw new Exception();
+        } else {
+            for (int i = 0; i < this.matrix.length; i++) {
+                for (int j = 0; j < this.matrix[0].length; j++) {
+                    matrix[i][j] -= m[i][j];
+                }
+            }
+        }
+        return matrix;
+    }
+
+    private int[][] identity() { //matricea identica
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+        return matrix;
+    }
+
+    private int[][] multiplyingNo(int x) throws Exception { //inmultim cu numar
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                matrix[i][j] *= x;
+            }
+        }
+        return matrix;
+    }
+
+    public MatrixClass multiplyingM(int r, int c, int[][] m) throws Exception { //inmultim cu matrice
+        MatrixClass result = new MatrixClass(c, row);
+        m = new int[r][c];
+        if (c != row) {
+            throw new Exception();
+        } else {
+            for (int i = 0; i < result.column; i++) {
+                for (int j = 0; j < result.row; j++) {
+                    for (int k = 0; k < matrix.length; k++) {
+                        result.matrix[i][j] += matrix[i][k] * m[k][j];
+                    }
+                }
+            }
+            return result;
+        }
+        
     }
 }
